394555a33957a5b542140d1300e1fe022522546f
Talha <mabbas.bese16seecs@seecs.edu.pk>
2019-07-16 01:40:29 +0500

    Item-Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import DisplayItem from "./components/DisplayItems"
+import DisplayItems from "./components/DisplayItems"
+import productSample from "./components/productSample"
+
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
+  constructor(){
+    super()
+    this.state={
+      products:[]
+    }
+  }
+
+  // sample is load as app component is mounted
+  componentDidMount(){
+    this.setState({
+      products : productSample
+    })
+  }
+
+  /**
+   * No NEED TO BUILD A SEPRATE FUNCTION TO LOAD DATA
+   */
+  // loadSamples(){
+  //   this.setState({
+  //     products : productSample
+  //   })
+  // }
+
+
+  render(){
   return (
-    <DisplayItem/>
+    <div>
+    <DisplayItems products={this.state.products}/>
+    </div>
   );
 }
-
+}
 export default App;
--- src/components/DisplayItems.js
+++ src/components/DisplayItems.js
@@ -1,10 +1,30 @@
 import React from "react";
+import Item from "./Item"
 
 class DisplayItems extends React.Component{
+constructor(){
+    super()
+    //this.renderProduct = this.renderProduct.bind(this);
+}
+
+renderProduct(){
+    //const array = this.props.products.products;
+    // let count =0;
+    // console.log("counter: ",count++,array);
+    // array.map((obj)=>{
+    //     return <Item key={obj.id}/>
+    // })
+    }
+
     render(){
+        const array = this.props.products.products;
+        let count =0;
+        //console.log("counter: ",count++,array);
+
         return(
             <div>
-                DisplayItems
+                {console.log("counter: ",count++,array)}
+                <Item obj={this.props.products.products}/>
             </div>
         )
     }
