88d07844dbbd69bc8bdd47197b6a47d18e99da40
Talha <mabbas.bese16seecs@seecs.edu.pk>
2019-07-16 10:17:18 +0500

    16/7/19 "Talha"

--- src/components/DisplayItems.js
+++ src/components/DisplayItems.js
@@ -4,27 +4,26 @@ import Item from "./Item"
 class DisplayItems extends React.Component{
 constructor(){
     super()
-    //this.renderProduct = this.renderProduct.bind(this);
+    this.renderProduct = this.renderProduct.bind(this);
 }
 
 renderProduct(){
-    //const array = this.props.products.products;
-    // let count =0;
-    // console.log("counter: ",count++,array);
-    // array.map((obj)=>{
-    //     return <Item key={obj.id}/>
-    // })
+    const array = this.props.products.products;
+    let count =0;
+    console.log("counter: ",count++,array);
+    array.map((key,obj)=>{
+        return <Item key={key} object={obj}/>
+    })
     }
 
     render(){
-        const array = this.props.products.products;
-        let count =0;
-        //console.log("counter: ",count++,array);
+       const array = this.props.products.products;
 
         return(
             <div>
-                {console.log("counter: ",count++,array)}
-                <Item obj={this.props.products.products}/>
+                <Item product={this.props.products.products}/>
+
+                {/* {this.renderProduct()} */}
             </div>
         )
     }
