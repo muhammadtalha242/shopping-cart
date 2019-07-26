7e28443c5a9ca286b013916689428db37597d0c0
Talha <mabbas.bese16seecs@seecs.edu.pk>
2019-07-26 14:04:48 +0500

    Updates

---
+++ precache-manifest.be9408058dddbc3226e10e4c14f6f98d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "240b5b2728137318d7b1e35172ac70d7",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "a035d6c0d6ebca863d55",
+    "url": "/shopping-cart/static/css/main.931a47d2.chunk.css"
+  },
+  {
+    "revision": "6b509cad2e9b6408bb0e",
+    "url": "/shopping-cart/static/js/2.32ded318.chunk.js"
+  },
+  {
+    "revision": "a035d6c0d6ebca863d55",
+    "url": "/shopping-cart/static/js/main.99724a82.chunk.js"
+  },
+  {
+    "revision": "3675f54918f9d621420d",
+    "url": "/shopping-cart/static/js/runtime~main.60a85d2f.js"
+  }
+]);
